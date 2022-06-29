42649cece53893b6a464a2d8602afa599bd32cc6
yotermidai <105938618+yotermidai@users.noreply.github.com>
2022-06-29 07:07:14 +0200

    Add files via upload

---
+++ Dneska JanSar/main.js
@@ -0,0 +1,18 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+myImage.onclick = function() 
+{
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc == 'images/34f5afaff5991d6e44af61b75b17a3fc.jpg') 
+	{
+      myImage.setAttribute('src','images/druhej20200529_161413.jpg');
+    } 
+	else 
+	{
+      myImage.setAttribute('src','images/34f5afaff5991d6e44af61b75b17a3fc.jpg');
+    }
+}
+
